7cf0a9efc44335b0d3ea402ab8f09c3567ddcce1
wmemcpy <celeste.francier@protonmail.com>
2023-09-25 15:47:40 +0200

    add GPU

--- postinstallation.sh
+++ postinstallation.sh
@@ -4,8 +4,7 @@ sudo -i
 
 function main() {
     echo "fastestmirror=true" >> /etc/dnf/dnf.conf
-    echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-    echo "countme=false" >> /etc/dnf/dnf.conf
+    echo "max_parallel_downloads=5" >> /etc/dnf/dnf.conf
 
     dnf clean all
     dnf upgrade -y
@@ -17,13 +16,27 @@ function main() {
     dnf install -y rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data 
     dnf install -y rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
 
+    read -r "Quel carte graphique avez-vous ? (intel, nvidia, amd) : " gpu
+    if [[ "${gpu}" == "intel" ]]; then
+        sudo dnf install intel-media-driver
+    elif [[ "${gpu}" == "nvidia" ]]; then
+        sudo touch /etc/dracut.conf.d/nvidia.conf
+        sudo echo "force_drivers+=" nvidia nvidia_modeset nvidia_uvm nvidia_drm "" >> /etc/dracut.conf.d/nvidia.conf
+        sudo dracut --force
+        sudo dnf install akmod-nvidia libva-vdpau-driver libva-utils xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-cuda vulkan
+    elif [[ "${gpu}" == "amd" ]]; then
+        sudo dnf install xorg-x11-drv-amdgpu
+    fi
+
     dnf install -y gstreamer1-plugins-{base,good,bad-free,good-extras,bad-free-extras,ugly-free} gstreamer1-libav
     dnf install -y gstreamer1-plugins-{bad-freeworld,ugly}
     dnf install -y libdvdcss
 
     dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
     dnf -y swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
-
+    dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+    dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+    dnf swap ffmpeg-free ffmpeg --allowerasing
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 }
 
